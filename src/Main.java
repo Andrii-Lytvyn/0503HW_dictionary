0a00d0571bb807a877c710375045db37d75ee2af
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-05 22:04:01 +0100

    Fix n2m. n->m

--- src/Main.java
+++ src/Main.java
@@ -44,9 +44,9 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
+    int m = Integer.parseInt(br.readLine());
     Map<String, String> dictionary = new HashMap<>();
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < m; i++) {
       String line = br.readLine();
       int space = line.indexOf(':');
       String term = line.substring(0, space);
